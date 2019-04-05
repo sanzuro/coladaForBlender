2f458d5c2a35535519177d48d7002ac6a95f9b08
sanzuro <42418726+sanzuro@users.noreply.github.com>
2019-04-05 19:43:42 +0530

    Add files via upload

--- colladatest/circleDefPoints.go
+++ colladatest/circleDefPoints.go
@@ -6,25 +6,42 @@ import (
 	"os"
 )
 
+// Point ...  the generated file would have 399 points
 type Point struct {
 	X, Y, Z float64
 }
 
 func main() {
 	file, err := os.Open("CPoints.sd")
+	// for creating file
+	// replace Open with Create
 	if err != nil {
 		fmt.Printf("sex happened")
 		panic(err)
 	}
 	defer file.Close()
+	i := 0
 	for x := float64(-1.0000); x < 1; x += float64(0.01) {
+		i++
 		var n Point
-		// n.x = x
-		// n.z = 0
-		// n.y = math.Sqrt(1 - x*x)
+		// n.X = x
+		// n.Z = 0
+		// n.Y = math.Sqrt(1 - x*x)
 		// binary.Write(file, binary.LittleEndian, &n)
+		// fmt.Printf(" %v %9.2v %v %v \n", i, n.X, n.Y, n.Z)
 		binary.Read(file, binary.LittleEndian, &n)
-		fmt.Printf("%v %v %v \n", n.X, n.Y, n.Z)
+		fmt.Printf("%v %v %v %v \n", i, n.X, n.Y, n.Z)
+	}
+	for x := float64(0.99); x > -1; x -= float64(0.01) {
+		var n Point
+		i++
+		// n.X = x
+		// n.Z = 0
+		// n.Y = -math.Sqrt(1 - x*x)
+		// binary.Write(file, binary.LittleEndian, &n)
+		// fmt.Printf("   %v %9.2v %v %v \n", i, n.X, n.Y, n.Z)
+		binary.Read(file, binary.LittleEndian, &n)
+		fmt.Printf("%v %v %v %v \n", i, n.X, n.Y, n.Z)
 	}
 
 }
--- colladatest/circlePointest.go
+++ colladatest/circlePointest.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
 )
+
+// this file has only has 399 values
+// tested
 
 func main() {
 	file, err := os.Open("CPoints.sd")
 	if err != nil {
 		panic(err)
 	}
-	var x []byte
-	file.Read(x)
-	fmt.Printf("%+v", x)
+	var x float64
+	for i := 0; i < 400; i++ {
+		binary.Read(file, binary.LittleEndian, &x)
+		fmt.Printf("%v ", x)
+		binary.Read(file, binary.LittleEndian, &x)
+		fmt.Printf("%v ", x)
+		binary.Read(file, binary.LittleEndian, &x)
+		fmt.Printf("%v \n", x)
+	}
 }
