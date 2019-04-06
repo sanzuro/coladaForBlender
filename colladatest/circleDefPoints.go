6e6c5690d2f2cc65cd137ab4607124c567b66a8d
sanzuro <42418726+sanzuro@users.noreply.github.com>
2019-04-07 00:10:20 +0530

    Add files via upload

--- colladatest/circleDefPoints.go
+++ colladatest/circleDefPoints.go
@@ -6,42 +6,16 @@ import (
 	"os"
 )
 
-// Point ...  the generated file would have 399 points
-type Point struct {
-	X, Y, Z float64
-}
-
 func main() {
-	file, err := os.Open("CPoints.sd")
-	// for creating file
-	// replace Open with Create
+	file, err := os.Create("Cpoints.sd")
 	if err != nil {
-		fmt.Printf("sex happened")
+		fmt.Printf("something is teribly teibly wrong with file not created ")
 		panic(err)
 	}
-	defer file.Close()
-	i := 0
-	for x := float64(-1.0000); x < 1; x += float64(0.01) {
-		i++
-		var n Point
-		// n.X = x
-		// n.Z = 0
-		// n.Y = math.Sqrt(1 - x*x)
-		// binary.Write(file, binary.LittleEndian, &n)
-		// fmt.Printf(" %v %9.2v %v %v \n", i, n.X, n.Y, n.Z)
-		binary.Read(file, binary.LittleEndian, &n)
-		fmt.Printf("%v %v %v %v \n", i, n.X, n.Y, n.Z)
-	}
-	for x := float64(0.99); x > -1; x -= float64(0.01) {
-		var n Point
-		i++
-		// n.X = x
-		// n.Z = 0
-		// n.Y = -math.Sqrt(1 - x*x)
-		// binary.Write(file, binary.LittleEndian, &n)
-		// fmt.Printf("   %v %9.2v %v %v \n", i, n.X, n.Y, n.Z)
-		binary.Read(file, binary.LittleEndian, &n)
-		fmt.Printf("%v %v %v %v \n", i, n.X, n.Y, n.Z)
+	var x float64
+	for i := 0; i < 786; i++ {
+		fmt.Scan(&x)
+		binary.Write(file, binary.LittleEndian, &x)
 	}
 
 }
--- colladatest/circlePointest.go
+++ colladatest/circlePointest.go
@@ -6,21 +6,17 @@ import (
 	"os"
 )
 
-// this file has only has 399 values
-// tested
-
 func main() {
-	file, err := os.Open("CPoints.sd")
-	if err != nil {
-		panic(err)
-	}
+	file, _ := os.Open("Cpoints.sd")
 	var x float64
-	for i := 0; i < 400; i++ {
+	for i := 0; i < 256; i++ {
+		fmt.Printf("%v ", i)
 		binary.Read(file, binary.LittleEndian, &x)
 		fmt.Printf("%v ", x)
 		binary.Read(file, binary.LittleEndian, &x)
 		fmt.Printf("%v ", x)
 		binary.Read(file, binary.LittleEndian, &x)
-		fmt.Printf("%v \n", x)
+		fmt.Printf("%v ", x)
+		fmt.Printf("\n")
 	}
 }
